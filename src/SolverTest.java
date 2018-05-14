31738c32b979bfa6bc015b66e0151d0bebb1791c
Sonali Sastry <sonali.sastry@gmail.com>
2018-05-13 22:35:56 -0400

    Committing initial Set game where cards have four fixed dimensions with three fixed values. Also committing .gitignore file
    Changing commit author to code owner (Sonali Sastry)

---
+++ src/SolverTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SolverTest {
+	/*  Future Unit Test for Solver class
+		cards[0] = new Card(Shape.SQUIGGLE, Color.RED, Number.ONE, Shading.OUTLINED);
+		cards[1] = new Card(Shape.DIAMOND, Color.RED, Number.ONE, Shading.OUTLINED);
+		cards[2] = new Card(Shape.OVAL, Color.RED, Number.ONE, Shading.OUTLINED);
+
+		for (int i=3; i< totalCardCount;  i++) {
+			cards[i] = new Card(Shape.getRandomShape(), Color.getRandomColor(),
+					Number.getRandomNumber(), Shading.getRandomShading());
+		}
+	 */
+
+	@Test
+	public void testGetCard() {
+		Card card = new Card(Shape.getRandomShape(), Color.RED,
+				Number.getRandomNumber(), Shading.getRandomShading());
+		assertEquals(Color.RED, card.getColor());
+
+	}
+
+}
