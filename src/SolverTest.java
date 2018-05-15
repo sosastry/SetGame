71aa5d89c893860ed070e40b6ed9033db47ccf09
Sonali Sastry <sonali.sastry@gmail.com>
2018-05-14 22:31:18 -0400

    Removed irrelevant test from Solver Test class

--- src/SolverTest.java
+++ src/SolverTest.java
@@ -6,16 +6,6 @@ import java.util.*;
 import static org.junit.Assert.*;
 
 public class SolverTest {
-	/*  Future Unit Test for Solver class
-		cards[0] = new Card(Shape.SQUIGGLE, Color.RED, Number.ONE, Shading.OUTLINED);
-		cards[1] = new Card(Shape.DIAMOND, Color.RED, Number.ONE, Shading.OUTLINED);
-		cards[2] = new Card(Shape.OVAL, Color.RED, Number.ONE, Shading.OUTLINED);
-
-		for (int i=3; i< totalCardCount;  i++) {
-			cards[i] = new Card(Shape.getRandomShape(), Color.getRandomColor(),
-					Number.getRandomNumber(), Shading.getRandomShading());
-		}
-	 */
 
     /* Generates few cards that should actually be a set*/
 	@Before
